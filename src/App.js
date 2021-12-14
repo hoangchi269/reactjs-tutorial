c46c9aa1e4cf323cd8c4a7ca77f465ddf660f724
hoangchi269 <hoangchi2691999@gmail.com>
2021-12-14 23:41:17 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Coca Cola",
+      amount: 10.5,
+      date: new Date(2021, 6, 14),
+    },
+    {
+      id: "e2",
+      title: "Pepsi",
+      amount: 15.5,
+      date: new Date(2021, 3, 24),
+    },
+    {
+      id: "e3",
+      title: "Fanta",
+      amount: 20.25,
+      date: new Date(2020, 9, 26),
+    },
+    {
+      id: "e4",
+      title: "New Tivi",
+      amount: 293.5,
+      date: new Date(2021, 9, 12),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
+  );
+}
+
+export default App;
